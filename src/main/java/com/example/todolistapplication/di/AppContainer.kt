07e1ba2d158ad5b0b369fe5ef07d2feeac33ed7e
Henok Asaye <henokasaye77@gmail.com>
2025-05-15 22:18:00 +0300

    add di files

---
+++ src/main/java/com/example/todolistapplication/di/AppContainer.kt
@@ -0,0 +1,29 @@
+package com.example.todolistapplication.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todolistapplication.data.TodoDatabase
+import com.example.todolistapplication.data.TodoRepository
+import com.example.todolistapplication.ui.screens.detail.TodoDetailViewModel
+import com.example.todolistapplication.ui.screens.list.TodoListViewModel
+
+class AppContainer(private val context: Context) {
+    private val database = Room.databaseBuilder(
+        context,
+        TodoDatabase::class.java,
+        "todo_database"
+    )
+    .fallbackToDestructiveMigration()
+    .build()
+
+    private val todoDao = database.todoDao()
+    private val todoRepository = TodoRepository(todoDao)
+
+    val todoListViewModel: TodoListViewModel by lazy {
+        TodoListViewModel(todoRepository)
+    }
+
+    fun createTodoDetailViewModel(): TodoDetailViewModel {
+        return TodoDetailViewModel(todoRepository)
+    }
+} 
