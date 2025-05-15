272de533d1ae03a25c6e08dba6fc9edf1f1d0535
Henok Asaye <henokasaye77@gmail.com>
2025-05-15 22:23:32 +0300

    add dependecies

--- src/main/java/com/example/todolistapplication/MainActivity.kt
+++ src/main/java/com/example/todolistapplication/MainActivity.kt
@@ -3,45 +3,30 @@ package com.example.todolistapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolistapplication.di.AppContainer
+import com.example.todolistapplication.ui.TodoNavigation
 import com.example.todolistapplication.ui.theme.TodoListApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var appContainer: AppContainer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        appContainer = AppContainer(applicationContext)
+
         setContent {
             TodoListApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TodoNavigation(appContainer)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoListApplicationTheme {
-        Greeting("Android")
     }
 }
